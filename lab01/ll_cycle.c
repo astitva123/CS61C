5d2a65d90f7bfef01a05fbccd73393f0c6c2b9a5
Astitva Sharma <astitvasharma@Astitvas-iMac.local>
2022-06-21 16:10:07 +0530

    Lab 1 completed

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *slow = head, *fast = head;
+
+    while(fast)
+    {
+        if(fast->next && fast->next->next)
+        {
+            fast = fast->next->next;
+        }
+        else 
+        {
+            return 0;
+        }
+
+        slow = slow->next;
+
+        if(fast == slow) return 1;
+    }
+
     return 0;
 }
