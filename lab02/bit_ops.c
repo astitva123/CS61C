e60d8eb2aed903f1f67f3a7f43a159dc7e3ce728
Astitva Sharma <astitvasharma@Astitvas-iMac.local>
2022-06-25 20:44:13 +0530

    lab02 completed

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,7 +9,8 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+
+    return (x&(1<<(n)))>>n;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
@@ -17,11 +18,18 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    (*x) = (*x)|(v<<n);
+    (*x) = (*x)&((~0)^(((~v)&1)<<n));
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    unsigned v = get_bit(*x, n);
+    v = ~v;
+    v = v&1;
+
+    set_bit(x, n, v);
 }
 
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,16 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t zero = ((*reg)&(1<<0))>>0;
+    uint16_t two = ((*reg)&(1<<2))>>2;
+    uint16_t three = ((*reg)&(1<<3))>>3;
+    uint16_t five = ((*reg)&(1<<5))>>5;
+
+    *reg = *reg>>1;
+
+    uint16_t msb = zero^two^three^five;
+
+    (*reg) = (*reg)|(msb<<15);
+    (*reg) = (*reg)&((~0)^(((~msb)&1)<<15));
 }
 
